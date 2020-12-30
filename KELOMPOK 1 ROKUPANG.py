56068f878eff4c00395e4cef788235e4dbfe2ed1
Pyandi-5566 <75074688+Pyandi-5566@users.noreply.github.com>
2020-12-30 22:30:59 +0700

    Update KELOMPOK 1 ROKUPANG.py
    menambahkan diskon dan nested if

--- KELOMPOK 1 ROKUPANG.py
+++ KELOMPOK 1 ROKUPANG.py
@@ -1,10 +1,10 @@
 print("============================================")
 print("KELOMPOK 1 ROKUPANG")
 print("PYANDI PRADIPTA SETIAWAN 19200126")
-print("ADE RIZKY ANWAR 19200247")
-print("SIDIK DWI NUGRAHA 19200061")
-print("DHEA AYU PERTIWI 19200602")
-print("HESTI ARIANINGTYAS 19200085")
+print("ADE RIZKY ANWAR          19200247")
+print("SIDIK DWI NUGRAHA        19200061")
+print("DHEA AYU PERTIWI         19200602")
+print("HESTI ARIANINGTYAS       19200085")
 print("============================================")
 
 
@@ -13,10 +13,10 @@ print("=================================")
 print("Kode       Varian Rasa      harga")
 print("=================================")
 print("1           Cokelat          10000")
-print("2           Strawberry       10000")
-print("3           Keju             10000")
-print("4           Bluberry         10000")
-print("5           Kacang           10000")
+print("2           Strawberry       11000")
+print("3           Keju             15000")
+print("4           Bluberry         11000")
+print("5           Kacang           10500")
 
 print("=============================================")
 nama_mhs = str(input("CUSTUMER = "))
@@ -41,25 +41,27 @@ while i<banyak_jenis:
         jumlah.append(banyak_potong[i]*int("10000"))
     elif kode_potong[i] == "2":
         jenis_potong.append("Strawberry")
-        harga.append("10000")
-        jumlah.append(banyak_potong[i]*int("10000"))
+        harga.append("11000")
+        jumlah.append(banyak_potong[i]*int("11000"))
     elif kode_potong[i] == "3":
         jenis_potong.append("keju")
-        harga.append("10000")
-        jumlah.append(banyak_potong[i]*int("10000"))
+        harga.append("15000")
+        jumlah.append(banyak_potong[i]*int("15000"))
     elif kode_potong[i] == "4":
         jenis_potong.append("Bluberry")
-        harga.append("10000")
-        jumlah.append(banyak_potong[i]*int("10000"))
+        harga.append("11000")
+        jumlah.append(banyak_potong[i]*int("11000"))
     elif kode_potong[i] == "5":
         jenis_potong.append("kacang")
-        harga.append("10000")
-        jumlah.append(banyak_potong[i]*int("10000"))
+        harga.append("10500")
+        jumlah.append(banyak_potong[i]*int("10500"))
     else:
         jenis_potong.append("Kode Salah")
         harga.append("0")
         jumlah.append(banyak_potong[i]*int("0"))
     i = i + 1
+
+
 
 print("                      ROKUPANG                ")
 print("=============================================================")
@@ -71,15 +73,26 @@ jumlah_bayar = 0
 a = 0
 while a < banyak_jenis:
     jumlah_bayar = jumlah_bayar + jumlah[a]
-    print("%i       %s        %s          %i          %i" % (a+1, jenis_potong[a], harga[a], banyak_potong[a], jumlah[a]))
+    print("%i        %s        %s        %i         %i" % (a+1, jenis_potong[a], harga[a], banyak_potong[a], jumlah[a]))
     a = a + 1
 
-pajak = jumlah_bayar * 0.1
+if jumlah_bayar >= 50000 :
+    diskon = jumlah_bayar * 0.05
+    if jumlah_bayar >= 100000 :
+        diskon = jumlah_bayar * 0.1
+else:
+    diskon = 0
+
+
+pajak = jumlah_bayar * 0.2
 total_bayar = jumlah_bayar + pajak
+total_bayar1 = total_bayar - diskon
+print("               Custumer            : ", nama_mhs)
 print("               Jumlah Bayar        Rp. ", jumlah_bayar)
-print("               Pajak 10%           Rp. ", pajak)
-print("               Total Bayar         Rp. ", total_bayar)
+print("               Pajak 20%           Rp. ", pajak)
+print("               Diskon 5 - 10%      Rp. ", diskon )
+print("               Total Bayar         Rp. ", total_bayar1)
 
 uang_tunai = int(input("UANG TUNAI : "))
-kembalian = uang_tunai - total_bayar
+kembalian = uang_tunai - total_bayar1
 print("KEMBALIAN = ", kembalian)
